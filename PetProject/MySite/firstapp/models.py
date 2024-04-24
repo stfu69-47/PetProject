c810ac6c1c18ec7139e427a90de0d7bd2d33f424
stfu69-47 <stfu69@yandex.ru>
2024-04-24 22:32:20 +0300

    Models.OneToOne.ManyToMany.OneToMany

--- PetProject/MySite/firstapp/models.py
+++ PetProject/MySite/firstapp/models.py
@@ -4,3 +4,33 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=25)
     age = models.IntegerField()
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=25)
+
+
+class Product(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    name = models.CharField(max_length=25)
+    price = models.IntegerField()
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=25)
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=25)
+    courses = models.ManyToManyField(Course)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=25)
+
+
+class Account(models.Model):
+    login = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
